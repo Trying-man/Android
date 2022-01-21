d51dee4a79f003088742978ea820757c8f8a517f
trying-man <zealousness@naver.com>
2022-01-22 01:37:01 +0900

    01.22

--- app/src/main/java/com/example/list2/MainActivity.java
+++ app/src/main/java/com/example/list2/MainActivity.java
@@ -87,8 +87,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            SingerItemView view = new SingerItemView(getApplicationContext());
-
+            SingerItemView view = null;
+            if(convertView == null) {
+                view = new SingerItemView(getApplicationContext());
+            }else {
+                view = (SingerItemView) convertView;
+            }
             SingerItem item = items.get(position);
             view.setName(item.getName());
             view.setMobile(item.getMobile());
